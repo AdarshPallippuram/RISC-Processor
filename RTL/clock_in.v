35a9032429e992a6924d6807f24becff9d42358e
AdarshPallippuram <adarshpallippuram2000@gmail.com>
2022-04-22 13:38:37 +0530

    Update clock_in.v

--- RTL/clock_in.v
+++ RTL/clock_in.v
@@ -1,20 +1,23 @@
 module stall_clk(
     input clk_in, stallb_en, rst,
-    output wire clk_f, clk_dcd, clk_exe);
-reg en_ff_1,en_ff_2,en_ff_3;
+    output wire clk_f, clk_dcd, clk_exe, clk_rf);
+reg en_ff_1,en_ff_2,en_ff_3,en_ff_4;
 assign clk_f=en_ff_1&clk_in;
 assign clk_dcd=en_ff_2&clk_in;
 assign clk_exe=en_ff_3&clk_in;
-always @(negedge clk_in or rst) begin
+assign clk_rf=en_ff_4&clk_in;
+always @(negedge clk_in or negedge rst) begin
     if(!rst) begin
-        en_ff_1<=1;
-        en_ff_2<=1;
-        en_ff_3<=1;
+        en_ff_1<=0;
+        en_ff_2<=0;
+        en_ff_3<=0;
+        en_ff_4<=0;
     end
     else begin
         en_ff_1<=stallb_en;
         en_ff_2<=en_ff_1;
         en_ff_3<=en_ff_2;
+        en_ff_4<=en_ff_3;
     end
 end
 endmodule
