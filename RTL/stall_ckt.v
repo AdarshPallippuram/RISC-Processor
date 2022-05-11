ef8a4bce168e4504eb2b6cec51ed02fe8e558168
AdarshPallippuram <adarshpallippuram2000@gmail.com>
2022-05-11 15:20:08 +0530

    Update stall_ckt.v

--- RTL/stall_ckt.v
+++ RTL/stall_ckt.v
@@ -1,5 +1,5 @@
-module stalling_design(clk,stall,pm_add,dm_add,rwb,fetch_clk,decode_clk,execute_clk,execute1_clk);
-input clk,stall,rwb;
+module stalling_design(clk,rst,stall,pm_add,dm_add,rwb,fetch_clk,decode_clk,execute_clk,execute1_clk);
+input clk,rst,stall,rwb;
 input[15:0]dm_add,pm_add;
 reg fetch_en=0,decode_en=0,execute_en=0,execute1_en=0;
 output wire fetch_clk,decode_clk,execute_clk,execute1_clk;
@@ -11,28 +11,41 @@ assign execute1_clk=execute1_en & clk;
 assign A[0]= (dm_add[15]|dm_add[14]|dm_add[13]|dm_add[12])&(~rwb);
 assign A[1]= (dm_add[15]|dm_add[14]|dm_add[13]|dm_add[12])&(rwb);
 assign A[2]= (pm_add[15]|pm_add[14]|pm_add[13]|pm_add[12]);
-always @(negedge clk)begin
-    fetch_en<=stall&~A[2];
-    decode_en<=(fetch_en&~(A[1]|A[0]))|(stall&(A[1]&A[0]));
-    execute_en<=(decode_en&!A[0])|(stall&A[0]);
-    execute1_en<=(execute_en&!A[0])|(stall&A[0]);
+always @(negedge clk or negedge rst)begin
+    if(!rst) begin
+        fetch_en<=1;
+        decode_en<=1;
+        execute_en<=1;
+        execute1_en<=1;
+    end
+    else begin
+        fetch_en<=stall;
+        decode_en<=fetch_en&~A[2];
+        execute_en<=decode_en&~A[1];
+        execute1_en<=execute1_en&~A[0];
+    end
 end
 endmodule
-/*
+
 module t_stalling_design();
-reg clk,stall,rwb;
+reg clk,rst,stall,rwb;
 reg[15:0] dm_add,pm_add ;
 wire  fetch_clk,decode_clk,execute_clk,execute1_clk;
-stalling_design inst(clk,stall,pm_add,dm_add,rwb,fetch_clk,decode_clk,execute_clk,execute1_clk);
+stalling_design inst(clk,rst,stall,pm_add,dm_add,rwb,fetch_clk,decode_clk,execute_clk,execute1_clk);
 initial begin 
     clk=1;
     forever #5 clk=~clk;
 end
-/*initial begin
+initial begin
     stall=1;
     #20 stall=0;
     #20 stall=1;
     #20 stall=0;
+end
+initial begin
+    rst =1;
+    #1 rst=0;
+    #2 rst=1;
 end
 initial begin
     rwb<=0;
@@ -59,7 +72,6 @@ initial begin
      #30 dm_add=16'h2FFF;
       #30 dm_add=16'h0FAC;
 end
-endmodule//
 
 initial begin
 	rwb=0;
@@ -85,4 +97,3 @@ initial begin
         #20 dm_add=16'h0ABC;
 end	
 endmodule
-*/
